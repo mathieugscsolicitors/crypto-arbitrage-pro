6e7f12723a9e201a39b3ae3e420ad2a4c859d552
mathieugscsolicitors <mathieu@gscsolicitors.uk>
2025-09-15 08:33:45 -0700

    Add critical file: src/pages/client/TransactionsPage.tsx

---
+++ src/pages/client/TransactionsPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { HistoryPage } from './HistoryPage';
+
+const TransactionsPage: React.FC = () => {
+  return <HistoryPage />;
+};
+
+export default TransactionsPage;

5332e12ff43460cc7948fedb00a36513efd47957
mathieugscsolicitors <mathieu@gscsolicitors.uk>
2025-09-15 08:33:42 -0700

    Add critical file: src/pages/client/DashboardPage.tsx

---
+++ src/pages/client/DashboardPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ClientDashboard } from './ClientDashboard';
+
+const DashboardPage: React.FC = () => {
+  return <ClientDashboard />;
+};
+
+export default DashboardPage;
